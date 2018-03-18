1a41999630fe90a76bc4920ebce7289f4820cdc5
Johannes Millan <johannes.millan@gmail.com>
2018-03-19 00:41:07 +0100

    feat display notification from storage

--- src/js/background.js
+++ src/js/background.js
@@ -4,13 +4,28 @@ import '../img/icon_128x128.png'
 const INTERVAL = 60 * 1000;
 let interval;
 
-function show() {
-  const title = 'TEST';
-  const msg = 'Time to make the toast.';
+function getRandomKey(obj) {
+  const keys = Object.keys(obj)
+  return keys[keys.length * Math.random() << 0];
+}
 
-  new Notification(title, {
-    body: msg,
+function show() {
+  let title = 'No vocabulary yet';
+  let msg = 'Click on the extension icon to change that.';
+
+  chrome.storage.sync.get(/* String or Array */null, function(items) {
+    const randomKey = getRandomKey(items);
+    const voc = randomKey;
+    const translation = items[randomKey];
+
+    title = voc;
+    msg = translation;
+
+    new Notification(title, {
+      body: msg,
+    });
   });
+
 }
 
 // Test for notification support.
