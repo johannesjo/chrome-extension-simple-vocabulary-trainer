f9025f11c630574a43855b95ad2a3a8ed41ca327
Johannes Millan <johannes.millan@gmail.com>
2018-03-19 00:34:04 +0100

    feat: save vocabulary

--- src/js/background.js
+++ src/js/background.js
@@ -1,22 +1,22 @@
 import '../img/icon_32x32.png'
 import '../img/icon_128x128.png'
 
-//const opt = {
-//  type: "list",
-//  title: "Primary Title",
-//  message: "Primary message to display",
-//  iconUrl: "url_to_small_icon",
-//  items: [{ title: "Item1", message: "This is item 1."},
-//    { title: "Item2", message: "This is item 2."},
-//    { title: "Item3", message: "This is item 3."}]
-//};
+const INTERVAL = 60 * 1000;
+let interval;
 
-const opt = {
-  type: 'basic',
-  title: 'Primary Title',
-  message: 'Primary message to display',
-  iconUrl: 'url_to_small_icon'
-};
-chrome.notifications.create(id, opt, ()=>{
-  console.log('shown');
-});
+function show() {
+  const title = 'TEST';
+  const msg = 'Time to make the toast.';
+
+  new Notification(title, {
+    body: msg,
+  });
+}
+
+// Test for notification support.
+if (window.Notification) {
+  interval = setInterval(() => {
+    show();
+  }, INTERVAL);
+  show();
+}
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,33 @@
 import "../css/popup.css";
-import hello from "./popup/example";
+import yml from 'js-yaml';
 
+const area = document.getElementsByTagName('textarea')[0];
+
+chrome.storage.sync.get(/* String or Array */null, function(items) {
+  const str = yml.safeDump(items);
+  console.log(str);
+  area.value = str;
+  console.log(items);
+});
+
+function updateVocabulary() {
+  const val = area.value;
+  let doc = false;
+
+  try {
+    doc = yml.safeLoad(val, 'utf8');
+    console.log(doc);
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (doc) {
+    localStorage.vocabulary = doc;
+    chrome.storage.sync.set(doc, function() {
+      console.log('SAVED');
+    });
+  }
+
+}
+
+area.addEventListener('input', updateVocabulary);
