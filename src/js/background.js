e1457eb8faa25aee5deddca72d027cc74a7a5436
Johannes Millan <johannes.millan@gmail.com>
2018-03-19 23:24:06 +0100

    feat: close vocabulary after timeout and on click

--- src/js/background.js
+++ src/js/background.js
@@ -5,27 +5,41 @@ const INTERVAL = 60 * 1000;
 let interval;
 
 function getRandomKey(obj) {
-  const keys = Object.keys(obj)
+  const keys = Object.keys(obj);
   return keys[keys.length * Math.random() << 0];
 }
+
+const NOTIFICATION_ID = 'NOTIFICATION_ID';
 
 function show() {
   let title = 'No vocabulary yet';
   let msg = 'Click on the extension icon to change that.';
 
-  chrome.storage.sync.get(/* String or Array */null, function(items) {
+  chrome.storage.sync.get(null, function(items) {
     const randomKey = getRandomKey(items);
     const voc = randomKey;
     const translation = items[randomKey];
 
     title = voc;
     msg = translation;
-
-    new Notification(title, {
-      body: msg,
+    chrome.notifications.create(NOTIFICATION_ID, {
+      type: 'list',
+      title: title,
+      message: '',
+      iconUrl: './icon_32x32.png',
+      items: [
+        { title: title, message: msg },
+      ]
     });
-  });
 
+    setTimeout(() => {
+      chrome.notifications.onClicked.addListener(close);
+    }, 10 * 1000);
+  });
+}
+
+function close() {
+  chrome.notifications.clear(NOTIFICATION_ID);
 }
 
 // Test for notification support.
